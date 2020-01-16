0f31b904f6c5dfcbc707174f419cc3bee322e9e2
Tyler Buck <57814333+thetylerb@users.noreply.github.com>
2020-01-16 18:59:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-//generate random password
 function generate(){
 
-    //set password length/complexity
     let complexity = 25
-    //possible password values
+
     let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
     let password = "";
 
-    //create for loop to choose password characters
     for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
-    //add password to textbox/display area
     document.getElementById("display").value = password;
 
-    //add password to previously generated passwords section
     document.getElementById("lastNums").innerHTML += password + "<br />";
 
 }
 
-//set default length display of 25
 document.getElementById("length").innerHTML = "Length: 25";
 
 
 
-//function to copy password to clipboard
 function copyPassword(){
 
     document.getElementById("display").select();
